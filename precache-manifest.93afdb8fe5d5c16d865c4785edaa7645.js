3274855694e0e4d3a21978fe182e22be6d0e5e02
Ezequiel Nielsen Hillen <ezenh87@gmail.com>
2024-08-28 16:11:40 -0300

    Updates

--- precache-manifest.93afdb8fe5d5c16d865c4785edaa7645.js
+++ precache-manifest.93afdb8fe5d5c16d865c4785edaa7645.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6cdfe791dc5633c1862d5f98569092e0",
+    "revision": "667979054e822ac67013c1abfb91f6bb",
     "url": "/tasks-with-react/index.html"
   },
   {
-    "revision": "68f2463c073ebd6a9843",
+    "revision": "2a1b803609c7dc86ca5d",
     "url": "/tasks-with-react/static/css/main.353b79d4.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/tasks-with-react/static/js/2.65dac575.chunk.js"
   },
   {
-    "revision": "68f2463c073ebd6a9843",
-    "url": "/tasks-with-react/static/js/main.2604d6eb.chunk.js"
+    "revision": "2a1b803609c7dc86ca5d",
+    "url": "/tasks-with-react/static/js/main.dc0cc710.chunk.js"
   },
   {
     "revision": "75fc30361a786cea0bf0",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tasks-with-react/precache-manifest.fca998cb81faec478a02ee8da24e915c.js"
+  "/tasks-with-react/precache-manifest.93afdb8fe5d5c16d865c4785edaa7645.js"
 );
 
 self.addEventListener('message', (event) => {
