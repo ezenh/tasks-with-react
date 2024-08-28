e90d87ac24469b3758a43757a8cc9ee926299b99
Ezequiel Nielsen Hillen <ezenh87@gmail.com>
2024-08-28 00:20:43 -0300

    Updates

---
+++ precache-manifest.9f0c8f974a958779e6c67759b48ce43a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "035c381f0f20d6335bb29152e2c3c06c",
+    "url": "/tasks-with-react/index.html"
+  },
+  {
+    "revision": "1a33c9b2d106ff105bb0",
+    "url": "/tasks-with-react/static/css/main.353b79d4.chunk.css"
+  },
+  {
+    "revision": "c9f72e472e9ea5139c8b",
+    "url": "/tasks-with-react/static/js/2.65dac575.chunk.js"
+  },
+  {
+    "revision": "1a33c9b2d106ff105bb0",
+    "url": "/tasks-with-react/static/js/main.1d6907bb.chunk.js"
+  },
+  {
+    "revision": "75fc30361a786cea0bf0",
+    "url": "/tasks-with-react/static/js/runtime~main.76b7118f.js"
+  }
+]);
